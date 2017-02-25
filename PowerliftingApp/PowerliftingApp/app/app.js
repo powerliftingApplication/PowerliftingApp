5227ec7be8c573b452115bf2156e4489bdac1822
drewedward - Andrew Young <young.andrew1991@gmail.com>
2017-02-25 18:45:41 -0500

    Importing project

---
+++ PowerliftingApp/PowerliftingApp/app/app.js
@@ -0,0 +1,108 @@
+/**
+ * @name AceApp
+ * @description
+ * # AceApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('AceApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+	//'angular-loading-bar',
+	'oc.lazyLoad',
+	'ui.bootstrap',
+	'ui.router',
+	'ace.directives',
+	'ngStorage'
+  ])
+  .config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider/**, cfpLoadingBarProvider*/) {
+      //cfpLoadingBarProvider.includeSpinner = true;
+
+      $urlRouterProvider.otherwise('/dashboard');
+
+        $stateProvider
+                .state('dashboard',
+                {
+                    url: '/dashboard',
+                    title: 'Dashboard',
+                    icon: 'fa fa-tachometer',
+
+                    templateUrl: 'app/views/pages/dashboard.html',
+                    controller: 'DashboardCtrl',
+
+                    resolve: {
+                        lazyLoad: [
+                            '$ocLazyLoad', function($ocLazyLoad) {
+                                return $ocLazyLoad.load([
+                                    {
+                                        name: 'angular-flot',
+                                        serie: true,
+                                        files: [
+                                            'components/Flot/jquery.flot.js',
+                                            'components/Flot/jquery.flot.pie.js',
+                                            'components/Flot/jquery.flot.resize.js',
+                                            'components/angular-flot/angular-flot.js'
+                                        ]
+                                    },
+                                    {
+                                        name: 'easypiechart',
+                                        files: ['components/_mod/easypiechart/angular.easypiechart.js']
+                                    },
+                                    {
+                                        name: 'AceApp',
+                                        files: ['app/controllers/pages/dashboard.js']
+                                    }
+                                ]);
+                            }
+                        ]
+                    }
+                })
+            //.state('profile',
+            //{
+            //    'abstract': true,
+            //    //url: '/form',
+            //    title: 'Profiles',
+            //    template: '<ui-view/>',
+            //    icon: 'fa fa-pencil-square-o'
+            //})
+            //.state('profile.powerlifting',
+            //{
+            //    url: '/profile-powerlifting',
+            //    title: 'Powerlifting Profile',
+
+            //    templateUrl: 'app/views/pages/powerliftingProfile.html',
+            //    controller: 'PowerliftingProfileCtrl',
+            //    resolve: {
+            //        lazyLoad: [
+            //            '$ocLazyLoad', function ($ocLazyLoad) {
+            //                return $ocLazyLoad.load([{
+            //                    name: 'angular-flot',
+            //                    serie: true,
+            //                    files: [
+            //                        'components/Flot/jquery.flot.js',
+            //                        'components/Flot/jquery.flot.pie.js',
+            //                        'components/Flot/jquery.flot.resize.js',
+            //                        'components/angular-flot/angular-flot.js'
+            //                    ]
+            //                },
+            //                    {
+            //                        name: 'sparkline',
+            //                        files: ['components/jquery.sparkline/index.js']
+            //                    },
+            //                    {
+            //                        name: 'AceApp',
+            //                        files: ['app/controllers/pages/powerliftingProfile.js']
+            //                    }
+            //                ]);
+            //            }
+            //        ]
+            //    }
+            //})
+            ;
+    })
+  .run(function ($rootScope) {
+
+  });
---
+++ PowerliftingApp/PowerliftingApp/app/controllers/pages/powerliftingProfile.js
@@ -0,0 +1,46 @@
+﻿angular.module('AceApp').controller('PowerliftingProfileCtrl', function($scope, $http, $timeout, $rootScope) {
+    $scope.message = "hello!";
+
+    //data for second flot chart
+    var d1 = [];
+    for (var i = 0; i < Math.PI * 2; i += 0.5) {
+        d1.push([i, Math.sin(i)]);
+    }
+    var d2 = [];
+    for (var i = 0; i < Math.PI * 2; i += 0.5) {
+        d2.push([i, Math.cos(i)]);
+    }
+    var d3 = [];
+    for (var i = 0; i < Math.PI * 2; i += 0.2) {
+        d3.push([i, Math.tan(i)]);
+    }
+    $scope.saleschart = {
+        'data': [
+            { label: "Domains", data: d1 },
+            { label: "Hosting", data: d2 },
+            { label: "Services", data: d3 }
+        ],
+        'options': {
+            hoverable: true,
+            shadowSize: 0,
+            series: {
+                lines: { show: true },
+                points: { show: true }
+            },
+            xaxis: {
+                tickLength: 0
+            },
+            yaxis: {
+                ticks: 10,
+                min: -2,
+                max: 2,
+                tickDecimals: 3
+            },
+            grid: {
+                backgroundColor: { colors: ["#fff", "#fff"] },
+                borderWidth: 1,
+                borderColor: '#555'
+            }
+        }
+    }
+});
