bc46db8b52eb7e6104525c1c9a4b375c5918c89b
drewedward - Andrew Young <young.andrew1991@gmail.com>
2017-02-25 23:27:35 -0500

    Initial check in for flight prioritization UI

--- PowerliftingApp/PowerliftingApp/app/app.js
+++ PowerliftingApp/PowerliftingApp/app/app.js
@@ -23,86 +23,116 @@ var app = angular
 
       $urlRouterProvider.otherwise('/dashboard');
 
-        $stateProvider
-                .state('dashboard',
-                {
-                    url: '/dashboard',
-                    title: 'Dashboard',
-                    icon: 'fa fa-tachometer',
+      $stateProvider
+              .state('dashboard',
+              {
+                  url: '/dashboard',
+                  title: 'Dashboard',
+                  icon: 'fa fa-tachometer',
 
-                    templateUrl: 'app/views/pages/dashboard.html',
-                    controller: 'DashboardCtrl',
+                  templateUrl: 'app/views/pages/dashboard.html',
+                  controller: 'DashboardCtrl',
 
-                    resolve: {
-                        lazyLoad: [
-                            '$ocLazyLoad', function($ocLazyLoad) {
-                                return $ocLazyLoad.load([
-                                    {
-                                        name: 'angular-flot',
-                                        serie: true,
-                                        files: [
-                                            'components/Flot/jquery.flot.js',
-                                            'components/Flot/jquery.flot.pie.js',
-                                            'components/Flot/jquery.flot.resize.js',
-                                            'components/angular-flot/angular-flot.js'
-                                        ]
-                                    },
-                                    {
-                                        name: 'easypiechart',
-                                        files: ['components/_mod/easypiechart/angular.easypiechart.js']
-                                    },
-                                    {
-                                        name: 'AceApp',
-                                        files: ['app/controllers/pages/dashboard.js']
-                                    }
-                                ]);
+                  resolve: {
+                      lazyLoad: [
+                          '$ocLazyLoad', function ($ocLazyLoad) {
+                              return $ocLazyLoad.load([
+                                  {
+                                      name: 'angular-flot',
+                                      serie: true,
+                                      files: [
+                                          'components/Flot/jquery.flot.js',
+                                          'components/Flot/jquery.flot.pie.js',
+                                          'components/Flot/jquery.flot.resize.js',
+                                          'components/angular-flot/angular-flot.js'
+                                      ]
+                                  },
+                                  {
+                                      name: 'easypiechart',
+                                      files: ['components/_mod/easypiechart/angular.easypiechart.js']
+                                  },
+                                  {
+                                      name: 'AceApp',
+                                      files: ['app/controllers/pages/dashboard.js']
+                                  }
+                              ]);
+                          }
+                      ]
+                  }
+              })
+        .state('flights',
+        {
+            'abstract': true,
+            title: 'Flights',
+            template: '<ui-view/>',
+            icon: 'fa fa-pencil-square-o'
+        })
+        .state('flights.prioritization',
+        {
+            url: '/flights-prioritization',
+            title: 'Prioritize Flights',
+
+            templateUrl: 'app/views/pages/flightPrioritization.html',
+            controller: 'FlightPrioritizationCtrl',
+            resolve: {
+                lazyLoad: [
+                    '$ocLazyLoad', function ($ocLazyLoad) {
+                        return $ocLazyLoad.load([{
+                            name: 'ui.tree',
+                            files: ['components/angular-ui-tree/angular-ui-tree.js']
+                        },
+                            {
+                                name: 'AceApp',
+                                files: ['app/controllers/pages/flightPrioritization.js']
                             }
-                        ]
+                        ]);
                     }
-                })
-            //.state('profile',
-            //{
-            //    'abstract': true,
-            //    //url: '/form',
-            //    title: 'Profiles',
-            //    template: '<ui-view/>',
-            //    icon: 'fa fa-pencil-square-o'
-            //})
-            //.state('profile.powerlifting',
-            //{
-            //    url: '/profile-powerlifting',
-            //    title: 'Powerlifting Profile',
+                ]
+            }
+        })
+      //.state('profile',
+      //{
+      //    'abstract': true,
+      //    //url: '/form',
+      //    title: 'Profiles',
+      //    template: '<ui-view/>',
+      //    icon: 'fa fa-pencil-square-o'
+      //})
+      //.state('profile.powerlifting',
+      //{
+      //    url: '/profile-powerlifting',
+      //    title: 'Powerlifting Profile',
 
-            //    templateUrl: 'app/views/pages/powerliftingProfile.html',
-            //    controller: 'PowerliftingProfileCtrl',
-            //    resolve: {
-            //        lazyLoad: [
-            //            '$ocLazyLoad', function ($ocLazyLoad) {
-            //                return $ocLazyLoad.load([{
-            //                    name: 'angular-flot',
-            //                    serie: true,
-            //                    files: [
-            //                        'components/Flot/jquery.flot.js',
-            //                        'components/Flot/jquery.flot.pie.js',
-            //                        'components/Flot/jquery.flot.resize.js',
-            //                        'components/angular-flot/angular-flot.js'
-            //                    ]
-            //                },
-            //                    {
-            //                        name: 'sparkline',
-            //                        files: ['components/jquery.sparkline/index.js']
-            //                    },
-            //                    {
-            //                        name: 'AceApp',
-            //                        files: ['app/controllers/pages/powerliftingProfile.js']
-            //                    }
-            //                ]);
-            //            }
-            //        ]
-            //    }
-            //})
-            ;
-    })
+      //    templateUrl: 'app/views/pages/powerliftingProfile.html',
+      //    controller: 'PowerliftingProfileCtrl',
+      //    resolve: {
+      //        lazyLoad: [
+      //            '$ocLazyLoad', function ($ocLazyLoad) {
+      //                return $ocLazyLoad.load([{
+      //                    name: 'angular-flot',
+      //                    serie: true,
+      //                    files: [
+      //                        'components/Flot/jquery.flot.js',
+      //                        'components/Flot/jquery.flot.pie.js',
+      //                        'components/Flot/jquery.flot.resize.js',
+      //                        'components/angular-flot/angular-flot.js'
+      //                    ]
+      //                },
+      //                    {
+      //                        name: 'sparkline',
+      //                        files: ['components/jquery.sparkline/index.js']
+      //                    },
+      //                    {
+      //                        name: 'AceApp',
+      //                        files: ['app/controllers/pages/powerliftingProfile.js']
+      //                    }
+      //                ]);
+      //            }
+      //        ]
+      //    }
+      //})
+      ;
+  })
   .run(function ($rootScope) {
 
   });
